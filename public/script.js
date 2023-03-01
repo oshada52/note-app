c84539193815a31302ba22e783cc8bd578dd0ec7
Dilshan Oshada <doshada32@gmail.com>
2023-03-01 15:37:54 +0530

    init commit

---
+++ public/script.js
@@ -0,0 +1,15 @@
+const button = document.getElementById("change-color-btn");
+
+button.addEventListener("click", () => {
+  const body = document.querySelector("body");
+  body.style.backgroundColor = getRandomColor();
+});
+
+function getRandomColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import express, { Request, Response, NextFunction } from "express";
+import path from "path";
+
+const app = express();
+
+//server static file
+app.use(express.static(path.join(__dirname, "../public")));
+
+// handle errors
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.log(err.stack);
+  res.status(500).send("Internal Server Error");
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public", "index.html"));
+});
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
